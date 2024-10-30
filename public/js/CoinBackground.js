10691b5fefcaebfcdd8ab9c3817bf9a39e38491e
RildoPaul <paulreynaga85@gmail.com>
2024-10-30 21:59:46 +0000

    Normalize line endings

---
+++ public/js/CoinBackground.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-undef */
+class CoinBackground extends React.Component {
+
+  render() {
+    return (
+      <div className="banner-sc">
+        <div className="tablo" id="p2">
+          <div className="tablo--1-ve-2 wow fade animated" style={{ "visibility": "visible" }}>
+            <div className="hero1 wow fadeInUp" data-wow-delay="0.8s"
+              style={{ "visibility": "visible", "animationDelay": "0.8s", "animationName": "fadeInUp" }} >
+              <img className="imagerotate1" src="./tablo/hero-cryptoce-1.png" alt="" />
+            </div>
+            <div className="hero2 wow fadeInUp" data-wow-delay="0.9s"
+              style={{ "visibility": "visible", "animationDelay": "0.9s", "animationName": "fadeInUp" }}>
+              <img className="imagerotate2" src="./tablo/hero-cryptoce-2.png" alt="" />
+            </div>
+            <div className="hero3 wow fadeInUp" data-wow-delay="1s"
+              style={{ "visibility": "visible", "animationDelay": "1s", "animationName": "fadeInUp" }}>
+              <img className="imagerotate3" src="./tablo/hero-cryptoce-3.png" alt="" />
+            </div>
+            <div className="hero4 wow fadeInUp" data-wow-delay="1.1s"
+              style={{ "visibility": "visible", "animationDelay": "1.1s", "animationName": "fadeInUp" }}>
+              <img className="imagerotate4" src="./tablo/hero-cryptoce-4.png" alt="" />
+            </div>
+            <div className="hero5 wow fadeInUp" data-wow-delay="1.2s"
+              style={{ "visibility": "visible", "animationDelay": "1.2s", "animationName": "fadeInUp" }}>
+              <img className="imagerotate5" src="./tablo/hero-cryptoce-5.png" alt="" />
+            </div>
+            <div className="hero6 wow fadeInUp" data-wow-delay="1.3s"
+              style={{ "visibility": "visible", "animationDelay": "1.3s", "animationName": "fadeInUp" }}>
+              <img className="imagerotate6" src="./tablo/hero-cryptoce-6.png" alt="" />
+            </div>
+            <div className="hero7 wow fadeInUp" data-wow-delay="1.4s"
+              style={{ "visibility": "visible", "animationDelay": "1.4s", "animationName": "fadeInUp" }}>
+              <img className="imagerotate7" src="./tablo/section3.png" alt="" />
+            </div>
+            <div className="hero8 wow fadeInUp" data-wow-delay="1.5s"
+              style={{ "visibility": "visible", "animationDelay": "1.5s", "animationName": "fadeInUp" }}>
+              <img className="imagerotate8" src="./img/tablo/section3.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
